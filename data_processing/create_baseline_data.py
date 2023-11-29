f81523eec4d13da94ea2ef633c482c144474be15
Dandan <daniel.leongsj@gmail.com>
2023-11-29 18:41:16 +0800

    refactor: abstraction of baseline data creation code

--- data_processing/create_baseline_data.py
+++ data_processing/create_baseline_data.py
@@ -1,5 +1,6 @@
 import glob 
-import os 
+import os
+import subprocess
 import soundfile as sf
 import numpy as np
 # training_datas = glob.glob("/home/emrys/G/zixun/espnet/egs2/asr_rl/dump_clean_8k/raw/org/data_src_train_8k/data/**/*.wav")
@@ -10,83 +11,85 @@ method = "g726"
 # method = "codec2"
 rat_noise, _ = sf.read("/home/emrys/G/zixun/espnet/egs2/asr_rl/dump_raw/rats_noise.wav")
 sr = 8000
-def augment(audio_path, method, outname=None):
-	audio_array, sr = sf.read(audio_path) 
-	if method == "add_rats_noise":
-		return audio_array+rat_noise[:len(audio_array)]
+
+def concat_audio(input_arr: np.ndarray, noise_arr: np.ndarray, max_length: int) -> np.ndarray:
+
+	if len(input_arr) > max_length:
+		output_arr = input_arr[:max_length]
+	elif input_arr < max_length:
+		output_arr = np.pad(input_arr, (0, max_length-len(input_arr)), mode="minimum")
+	else:
+		output_arr = input_arr
+	
+	output_arr = output_arr + noise_arr[:max_length]
+	return output_arr
+
+
+def simulate_g726(input_path: str, output_path: str, arr_noise: np.ndarray, target_sr: int = 8000) -> np.ndarray:
+	# ffmpeg will error out if input and output paths are the same
+	temp1_path, temp2_path, temp3_path = "temp1.wav", "temp2.wav", "temp3.wav"
+
+	commands = [
+		f"ffmpeg -hide_banner -loglevel error -i {input_path} -ar {target_sr} -ac 1 -y {temp1_path}",
+		f"ffmpeg -hide_banner -loglevel error -i {temp1_path} -acodec g726 -b:a 16000 {temp2_path}",
+		f"ffmpeg -hide_banner -loglevel error -i {temp2_path} -ar {target_sr} -y {temp3_path}",
+	]
+
+	for command in commands:
+		subprocess.run(command.split(" "))
+
+	arr, _ = sf.read(input_path)
+	# ensure array is mono-channelled
+	if np.dim(arr) > 1:
+		arr = np.mean(arr, axis=1)
+
+	arr_g726, sr = sf.read(temp3_path)
+	assert sr == target_sr, f"input sr ({sr}) and target sr ({target_sr}) mismatch."
+
+	out = concat_audio(arr_g726, arr_noise, len(arr))
+	sf.write(output_path, out, target_sr)
+	subprocess.run(["rm", temp1_path, temp2_path, temp3_path])
+	
+	return out
+
+def simulate_codec2(input_path: str, output_path: str, arr_noise: np.ndarray, target_sr: int = 8000) -> np.ndarray:
+
+	# ffmpeg will error out if input and output paths are the same
+	temp1_path, temp2_path, temp3_path, temp4_path = "temp1.raw", "temp2.bit", "temp3.raw", "temp4.wav"
+	br = "700c"
+
+	commands = [
+		f"ffmpeg -hide_banner -loglevel error -i {input_path} -f s16le -ar {target_sr} -ac 1 -acodec pcm_s16le {temp1_path}",
+		f"c2enc {br} {temp1_path} {temp2_path}",
+		f"c2dec {br} {temp2_path} {temp3_path}",
+		f"ffmpeg -hide_banner -loglevel error -f s16le -ar {target_sr} -ac 1 -i {temp3_path} {temp4_path}",		
+	]
+
+	for command in commands:
+		subprocess.run(command.split(" "))
+
+	arr, _ = sf.read(input_path)
+	# ensure array is mono-channelled
+	if np.dim(arr) > 1:
+		arr = np.mean(arr, axis=1)
+
+	arr_codec2, sr = sf.read(temp4_path)
+	assert sr == target_sr, f"input sr ({sr}) and target sr ({target_sr}) mismatch."
+
+	out = concat_audio(arr_codec2, arr_noise, len(arr))
+	sf.write(output_path, out, target_sr)
+	subprocess.run(["rm", temp1_path, temp2_path, temp3_path, temp4_path])
+
+	return out
+
+def augment(audio_path, method):
 	if method == "g726":
-		inter1 = "inter1.wav"
-		inter2 = "inter2.wav"
-		inter3 = "inter3.wav"
-		command1 = f"ffmpeg -hide_banner -loglevel error -i {audio_path} -ar 8k -y {inter1}"
-
-		command2 = f"ffmpeg -hide_banner -loglevel error -i {inter1} -acodec g726 -b:a 16k {inter2}"
-
-		command3 = f"ffmpeg -hide_banner -loglevel error -i {inter2} -ar 8k -y {inter3}"
-
-		command4 = f"rm {inter1} {inter2} {inter3}"
-
-		os.system(command1)
-		os.system(command2)
-		os.system(command3)
-		audio_array_g726, sr = sf.read(inter3) 
-		os.system(command4)
-		if len(audio_array_g726)>len(audio_array):
-			audio_array_g726 = audio_array_g726[:len(audio_array)]
-			print("padded")
-		elif len(audio_array_g726)<len(audio_array):
-			audio_array_g726 = audio_array
-			print("werid")
-
-		# print(audio_array_g726.shape,audio_array.shape )
-		out = audio_array_g726+rat_noise[:len(audio_array)]
-		return out 
-	if method == "codec2":
-		inter1 = "inter1.raw"
-		inter2 = "test_4_5.bit"
-		inter3 = "test_4_5.raw"
-		inter4 = "test_4_5.wav"
-		br = "700C"
-		# br = "2400"
-
-		command1 = f"ffmpeg -hide_banner -loglevel error -i {audio_path} -f s16le -ar 8k -acodec pcm_s16le {inter1}" # -ar 16k
-
-		# /home/emrys/G/zixun/espnet/egs2/asr_rl/codec2/build_linux/src
-		command2 = f"codec2/build_linux/src/c2enc {br} {inter1} {inter2}"
-		# command2_ = f"codec2/build_linux/src/c2enc {br} test.raw {inter2}"
-
-		command3 = f"codec2/build_linux/src/c2dec {br} {inter2} {inter3}"
-		command4 = f"ffmpeg -hide_banner -loglevel error -f s16le -ar 8k -ac 1 -i {inter3} {inter4} "
-
-		os.system(command1)
-		os.system(command2)
-
-		# if not os.path.exists("test.raw"):
-		# 	os.system(command2)
-		# else:
-		# 	print("hha")
-		# 	os.system(command2_)
-		os.system(command3)
-		os.system(command4)
-
-		audio_array_g726, sr = sf.read(inter4) 
-		command5 = f"rm {inter1} {inter2} {inter3} {inter4}"
-
-		os.system(command5)
-
-		if len(audio_array_g726)>len(audio_array):
-			audio_array_g726 = audio_array_g726[:len(audio_array)]
-			print("padded")
-		elif len(audio_array_g726)<len(audio_array):
-			audio_array_g726 = np.pad(audio_array_g726, (0, len(audio_array)-len(audio_array_g726)))
-			print("werid")
-		out = audio_array_g726+rat_noise[:len(audio_array)]
-		# out = audio_array_g726
-
-		return out 
-	# 	cmd = f""
-
-
+		return simulate_g726(audio_path, audio_path.replace(".wav", "_out.wav"), rat_noise, 8000)
+	elif method == "codec2":
+		return simulate_codec2(audio_path, audio_path.replace(".wav", "_out.wav"), rat_noise, 8000)
+	else:
+		audio_array, _ = sf.read(audio_path)
+		return audio_array+rat_noise[:len(audio_array)]
 
 # for training_data in training_datas:
 # 	new_dir = f"/home/emrys/G/zixun/espnet/egs2/asr_rl/dump_raw/raw/org/data_src_train_8k_{method}/data/"+training_data.split("/")[-2] 
